5b6cf9f1d527a097f55d178924844b359eccc948
AniaJezierska <candyrockstore5@gmail.com>
2020-11-29 20:58:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 export AUTH0_DOMAIN='dev-i7v2mtub.eu.auth0.com'
 export API_AUDIENCE='casting'
 export ALGORITHMS=['RS256']
-export DATABASE_URI = "postgres://{}/{}".format('localhost:8081', database_name)
+export DATABASE_URI = "postgres://{}/{}".format('localhost:5432', database_name)
